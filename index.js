7c219586f36bc7c15e73f8aeaf25d344809a08e7
mateus-mqs <mateusmarques447@gmail.com>
2021-05-11 18:53:18 -0300

    removing bot token

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const ytdl = require('ytdl-core');
 
-const token = "bottoken";
+const token = "botoken";
 bot.login(token);
 var mensagem = [];
 
